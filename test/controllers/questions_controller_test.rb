dd4bc3724e072f1604c84ff8d30a8825a90e6624
Mike Limansky <mike.limansky@gmail.com>
2015-01-18 02:09:53 +0300

    Fix questions controller test.

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
+  fixtures :tests
+
   test "should get new" do
-    get :new
+    get :new, test_id: tests(:math_test).id
     assert_response :success
   end
 
